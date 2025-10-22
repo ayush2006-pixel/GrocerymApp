449da2fdd73383394897e8b8125ae5b6073e652d
Ayush_Sri <ayushsrivastava192006@gmail.com>
2025-10-23 03:01:46 +0530

    Finnaally Hoisted on Vercel

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,6 +6,6 @@ const userRoute = express.Router();
 userRoute.post("/register" , register);
 userRoute.post("/login" , login);
 userRoute.get("/is-auth" , AuthUser ,isAuth);
-userRoute.get("/logout" , AuthUser , logout);
+userRoute.get("/logout" , AuthUser, logout);
 
 export default userRoute;
