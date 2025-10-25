19a9c98712f2e6935a5b2e7f1c4454268beea037
Ayush_Sri <ayushsrivastava192006@gmail.com>
2025-10-25 22:45:47 +0530

    idk but t should be the 11th or 12th attempth to make it perfect

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,6 +6,6 @@ const userRoute = express.Router();
 userRoute.post("/register" , register);
 userRoute.post("/login" , login);
 userRoute.get("/is-auth" , AuthUser ,isAuth);
-userRoute.get("/logout" , AuthUser, logout);
+userRoute.post("/logout" , AuthUser, logout);
 
 export default userRoute;
